298b6310be1dc0dd0ec2e88be5b1a86401f0c4c9
Andresito de Guzman <andresitomyemail@gmail.com>
2017-08-10 09:42:50 +0800

    Added comments

--- public/_interface/_common/js_global_declaratives.php
+++ public/_interface/_common/js_global_declaratives.php
@@ -1,3 +1,6 @@
+	// Declare Error
 	var error = "<div class='container'><br><br><center><h4 class='grey-text'><i class='medium material-icons'>warning</i><br>You are offline</h4></center></div>";
+	// Declare Loading
 	var loading = "<div class='progress green lighten-4'><div class='indeterminate seagreen'></div></div>";
+	// Declare Ajax Loader
     var loadingCircle = "<center><div class='preloader-wrapper small active'><div class='spinner-layer spinner-green-only'> <div class='circle-clipper left'><div class='circle'></div></div> <div class='gap-patch'><div class='circle'></div></div> <div class='circle-clipper right'><div class='circle'></div></div> </div></div></center>";
