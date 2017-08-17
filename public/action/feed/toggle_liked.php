5006a8c380826da58be494543433e1f278732ce7
Andresito de Guzman <andresitomyemail@gmail.com>
2017-08-17 15:42:45 +0800

    Removed Unused Attendance

--- public/action/feed/toggle_liked.php
+++ public/action/feed/toggle_liked.php
@@ -8,56 +8,93 @@ session_start();
 
 // Declare Permission Level
 $perm = 3;
+
+// Require Secure File
 require_once("../../_system/secure.php");
 
 // Include Database File
 include("../_require/db.php");
 
+// Set initial value
 $do = False;
 
-if(empty($_SESSION['user_id'])){
-    $do = False;
-} else {
+// Check if session not empty
+if(!empty($_SESSION['user_id'])){
+
     $user_id = $_SESSION['user_id'];
     $do = True;
+
 }
 
+// Check if empty post id
 if(empty($_REQUEST['post_id'])){
+
     $do = False;
+
 } else {
+
     $post_id = $_REQUEST['post_id'];
     $do = True;
+
 }
 
+// Finally
 if($do === True){
-    $check_post_exists = $db_post->get("post_id","post_id", "$post_id");
-    if(empty($check_post_exists)){
-    } else {
 
-     $check_exists = $db_postlikes->where(array(), "post_id", "$post_id");
-     $count = 0;
-     if(empty($check_exists)){
-         $count = 0;
-     } else {
-         foreach($check_exists as $like){
-             $postlike_id = $like['postlike_id'];
-             $user_id_now = $like['user_id'];
-             if($user_id == $user_id_now){
-                 $count = $count++;
-                 $index = $db_postlikes->index("postlike_id", "$postlike_id");
-                 $db_postlikes->rm($index);
-             }
-         }
-     }
+    //  Check if post exists by query
+    $check_post_exists = $db_post->get("post_id","post_id", "$post_id");
+
+    // Check if not empty
+    if(!empty($check_post_exists)){
+
+        // Get array
+        $check_exists = $db_postlikes->where(array(), "post_id", "$post_id");
+
+        // Set initial value
+        $count = 0;
+
+        // Check if empty 
+        if(!empty($check_exists)){
+
+            // Loop along array
+            foreach($check_exists as $like){
+
+                // Set val
+                $postlike_id = $like['postlike_id'];
+                $user_id_now = $like['user_id'];
+                
+                // Check if match w/ user ID
+                if($user_id == $user_id_now){
+
+                    // Increment if found
+                    $count = $count++;
+
+                    // Get ID
+                    $index = $db_postlikes->index("postlike_id", "$postlike_id");
+
+                    // Remove from DB
+                    $db_postlikes->rm($index);
+                
+                }
+
+            }
+
+        }
 
     }
 
 
+    // check if empty count
     if(empty($count)){
-        // Add
+
+        // Generate a Unique ID
         $postlike_id = uniqid();
+
+        // Get date and time
         $date = date("M d Y");
         $time = date("h:i a");
+
+        // Construct Array
         $array = array(
             "postlike_id" => "$postlike_id",
             "user_id" => "$user_id",
@@ -65,8 +102,11 @@ if($do === True){
             "date" => "$date",
             "time" => "$time"
         );
+
+        // Add to DB
         $db_postlikes->add($array);
-     }
+
+    }
 
 }
 
