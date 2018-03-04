3441e5d02cb5f631eaaeb6be964c86f5ef77bc92
Andresito de Guzman <deguzman.andresito@gmail.com>
2018-03-04 09:53:37 +0800

    Update myid.php

--- public/_interface/_common/myid.php
+++ public/_interface/_common/myid.php
@@ -2,7 +2,7 @@
   		<div class="modal-content">
   			<center>
   				<a href="https://chart.googleapis.com/chart?cht=qr&chs=300x300&chl=<?=$student_id?>" target="_blank" rel="noopener">
-  					<img src="https://chart.googleapis.com/chart?cht=qr&chs=300x300&chl=https://hcmontessori.000webhostapp.com/forms/ecash/transact.php?student_id=<?=$student_id?>" class='responsive-img round'>
+  					<img src="https://chart.googleapis.com/chart?cht=qr&chs=300x300&chl=https://hcmontessori.000webhostapp.com/pay.php?student_id=<?=$student_id?>" class='responsive-img round'>
   				</a>
   				<p class="white-text">
   					<font size="4pt"><?=$student_id?></font><br>
