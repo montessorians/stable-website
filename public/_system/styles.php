7d98d3444a9316c7dd81126f03807704e9262818
Andresito de Guzman <andresitomyemail@gmail.com>
2019-06-17 12:37:42 +0800

    attempt to fix jquery integrity issue

--- public/_system/styles.php
+++ public/_system/styles.php
@@ -103,13 +103,17 @@
     <script
       type="text/javascript"
       src="/assets/js/vue.min.js">
-    </script>
+    </script> 
     <script
+      type="text/javascript"
+      src="https://code.jquery.com/jquery-3.2.1.min.js">
+    </script>
+    <!--script
       type="text/javascript"
       src="https://code.jquery.com/jquery-3.2.1.min.js"
       integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
       crossorigin="anonymous">
-    </script>
+    </script-->
     <script
       type="text/javascript"
       src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/js/materialize.min.js">
