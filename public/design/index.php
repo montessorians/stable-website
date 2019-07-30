22f5ae6174bdbd23e3fee94fe67bca1ad8aba064
Andresito de Guzman <andresitomyemail@gmail.com>
2019-07-30 15:00:45 +0800

    Edits

--- public/design/index.php
+++ public/design/index.php
@@ -1,20 +1,22 @@
-<?php
-require_once("../_system/config.php");
-?>
+<?php require_once("../_system/config.php"); ?>
 <!Doctype html>
 <html lang="en">
     <head>
+        <title>Design Language - <?=$site_title?></title>
         <?php require_once("../_system/styles.php"); ?>
     </head>
     <body style="background-color:#EBFDF3; font-family: 'Rubik', sans-serif !important;">
-        <nav class="seagreen z-depth-0" style="border-radius: 0px 0px 25px 25px; height: 150px">
+        <nav class="seagreen z-depth-0" style="border-radius: 0px 0px 30px 30px; height: 150px">
             <center>
-            <img src="https://hcmontessori.000webhostapp.com/assets/imgs/logo.jpg" width="80px" style="margin-top:10px">
+            <img src="https://hcmontessori.000webhostapp.com/assets/imgs/logo.jpg" width="80px" style="border-radius: 10px; margin-top:10px" margin-bottom:0; padding:0;>
+            <h4 style="margin:0; padding: 0; line-height:0">Design Language</h4>
             </center>
         </nav><br><br>
         <div class="container">
-            <h5 class="seagreen-text">Philosophy and Disciplines</h5>
-            <h5 class="seagreen-text">Elements</h5>
+            <h4 class="seagreen-text">Philosophy and Disciplines</h4>
+                <h5>Child </h5>
+
+            <h4 class="seagreen-text">Elements</h4>
         </div>
     </body>
 </html>
