c902913fabe0abe2fae7da54efb232c48254bc12
Andresito de Guzman <andresitomyemail@gmail.com>
2019-07-30 14:40:26 +0800

    Create index.php

---
+++ public/design/index.php
@@ -0,0 +1,20 @@
+<?php
+require_once("../_system/config.php");
+?>
+<!Doctype html>
+<html lang="en">
+    <head>
+        <?php require_once("../_system/styles.php"); ?>
+    </head>
+    <body style="background-color:#EBFDF3; font-family: 'Rubik', sans-serif !important;">
+        <nav class="seagreen z-depth-0" style="border-radius: 0px 0px 25px 25px; height: 150px">
+            <center>
+            <img src="https://hcmontessori.000webhostapp.com/assets/imgs/logo.jpg" width="80px" style="margin-top:10px">
+            </center>
+        </nav><br><br>
+        <div class="container">
+            <h5 class="seagreen-text">Philosophy and Disciplines</h5>
+            <h5 class="seagreen-text">Elements</h5>
+        </div>
+    </body>
+</html>
