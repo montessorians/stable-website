e85185ba91d6cbc369f3d9e2029cd0df66a3ef3c
Andresito de Guzman <andresitomyemail@gmail.com>
2019-07-31 05:47:42 +0000

    Added design

--- public/design/index.php
+++ public/design/index.php
@@ -14,9 +14,70 @@
         </nav><br><br>
         <div class="container">
             <h4 class="seagreen-text">Philosophy and Disciplines</h4>
-                <h5>Child </h5>
-
+            <br>
+                <h5>Children are vulnerable by nature</h5>
+                <p>
+                    As an institution that fosters growth, we understand that child development is complex.
+                    Students have not reached their full cognitive capability during their early years.
+                    In designing interfaces and physical structures, it is important to consider that they should be immersed in safe environments.
+                    Staying away from sharp objects or anything that can physically harm them should be avoided at best.
+                    To have a coherent experience across the institution, a design language was made to create a consistent look and feel to the brand and digital experiences.<br>
+                    <br>
+                    It is important to minimize sharp edges, all design elements are now rounded including shapes and even the typography.
+                    Also, the color is kept consistent and easy on the eyes so that people from all ages can easily use and read what's on the screen or print.
+                </p>
+            <br>
+            <br>
+            <div class="row">
+                <div class="col s4">
+                    <div style="width:100px; height: 100px; border-radius: 30px;" class="seagreen"></div>
+                </div>
+                <div class="col s4">
+                    <div style="width:100px; height: 100px; border-radius: 15px;" class="seagreen"></div>
+                </div>
+                <div class="col s4">
+                    <div style="width:100px; height: 100px; border-radius: 30px 12px 30px 12px;" class="seagreen"></div>
+                </div>
+            </div>
+            <br>
+            <h4>This is a sample text.</h4>
+            <br>
             <h4 class="seagreen-text">Elements</h4>
+            <br>
+                <h5>Typography</h5>
+                <p>We use Rubik as the official font for all our digital and print assets. Check it out on <a class="seagreen-text" href="https://fonts.google.com/specimen/Rubik" target="_blank">Google Fonts</a></p>
+                    <h1>Sample Typography</h1>
+                    <h2>Sample Typography</h2>
+                    <h3>Sample Typography</h3>
+                    <h4>Sample Typography</h4>
+                    <h5>Sample Typography</h5>
+                <br><br>
+                <h5>Color Palette</h5>
+                <p>As much as possible, we limit the colors to maintain the brand and make it easy for everyone to understand our digital and print assets. We use seagreen (#2e8b57), a websafe color, heavily across all our designs. </p><br>
+                <div class="row">
+                    <div class="col s4">
+                        <div style="width:100px; height: 100px; border-radius: 30px;" class="seagreen"></div>
+                        <p>Seagreen<br>#2e8b57</p>
+                    </div>
+                    <div class="col s4">
+                        <div style="width:100px; height: 100px; border-radius: 30px; background-color:#EBFDF3; border-width:1px; border-style:solid"></div>
+                        <p>Light Seagreen<br>#EBFDF3</p>
+                    </div>
+                    <div class="col s4">
+                        <div style="width:100px; height: 100px; border-radius: 30px; background-color:white; border-width:1px; border-style:solid"></div>
+                        <p>White<br>#FFF</p>
+                    </div>
+                    <div class="col s4">
+                        <div style="width:100px; height: 100px; border-radius: 30px; background-color:black;"></div>
+                        <p>Black<br>#000</p>
+                    </div>
+                </div>
+                
+                <br><br>
+                <h5>Iconography</h5>
+                <p>Currently, we use two icon packs. For our photo and video adverts, we use <a href="http://www.evericons.com/" target="_blank" class="seagreen-text">Evericons</a>. Meanwhile, for apps and websites we use <a href="material.io/icons" target="_blank" class="seagreen-text">material icons</a>.</p>
+
+            <br><br><br><br><br>
         </div>
     </body>
 </html>
